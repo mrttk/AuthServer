bb9076d6fb12c67d60d1c54e29527a9885eaafd5
= <=>
2021-12-10 16:52:17 +0200

    Add AuthenticationService interface and relational classes

---
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,18 @@
+﻿using AuthServer.Core.DTOs;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<Responce<TokenDto>> CreateTokenAsync(LoginDto login);
+        Task<Responce<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+        Task<Responce<NoDataDto>> RevokeRefreshToken(string refreshToken);
+        Task<Responce<ClientTokenDto>> CreateTokenByClient(ClientTokenDto clientTokenDto);
+    }
+}
