1e03ffb9142a1e1986c6593575235bb639b84bdc
= <=>
2021-12-16 01:12:05 +0200

    Add token based authentication for mini app's

--- AuthServer.API/Controllers/ProductController.cs
+++ AuthServer.API/Controllers/ProductController.cs
@@ -41,7 +41,7 @@ namespace AuthServer.API.Controllers
             return ActionResultInstance(await _productService.Update(productDto,productDto.Id));
         }
 
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public async Task<IActionResult>DeleteProduct(int id)
         {
             return ActionResultInstance(await _productService.Remove(id));
