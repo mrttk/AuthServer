c8e18bb7390b59e2b59890753b9c8948802cb449
= <=>
2021-12-16 01:52:14 +0200

    Add controllers for mini apps

---
+++ MiniApp2.API/Controllers/InvoiceController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MiniApp2.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        public IActionResult GetInvoice()
+        {
+            var userName = HttpContext.User.Identity.Name;
+            var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+            //query with userId or userName from database
+
+            return Ok($"Invoice operations => User Name:{userName}-User Id:{userId}");
+        }
+    }
+}
