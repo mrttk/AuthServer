7b52e12242dc8ea4061736aa46d3424460b93d5d
= <=>
2021-12-15 13:28:28 +0200

    Add UseAuthentication method for pipeline.

--- AuthServer.API/Startup.cs
+++ AuthServer.API/Startup.cs
@@ -105,6 +105,8 @@ namespace AuthServer.API
 
             app.UseRouting();
 
+            app.UseAuthentication();
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
