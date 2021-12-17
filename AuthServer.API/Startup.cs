3b40be1d2382208cc0312b00b9b2f17283417133
= <=>
2021-12-17 20:45:40 +0200

    Add Custom Exception

--- AuthServer.API/Startup.cs
+++ AuthServer.API/Startup.cs
@@ -108,6 +108,10 @@ namespace AuthServer.API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AuthServer.API v1"));
             }
+            else
+            {
+                app.UseCustomException();
+            }
 
             app.UseHttpsRedirection();
 
